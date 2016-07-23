8d73e8639bcc7937723a8e2e1d0c7ee2b080cad9
wangfm <164245260@qq.com>
2016-07-23 10:26:38 +0800

    c add user.html

--- Blog/app.py
+++ Blog/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask_script import Manager
 
 app = Flask(__name__)
@@ -8,6 +8,10 @@ manager = Manager(app)
 @app.route('/')
 def index():
     return 'Blog'
+
+@app.route('/user/<name1>')
+def user(name1):
+    return render_template('user.html',name2=name1)
 
 
 if __name__ == '__main__':
