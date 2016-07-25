1cbbb6d1d506ccf47a2e780aa31816a4aedbcf5a
wangfm <164245260@qq.com>
2016-07-25 16:41:44 +0800

    add

--- Blog/app.py
+++ Blog/app.py
@@ -19,17 +19,11 @@ manager = Manager(app)
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 
-
-class NameForm(Form):
-    name = StringField('name', validators=[Required()])
-    submit = SubmitField('submit')
-
-
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -44,15 +38,28 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+class NameForm(Form):
+    name = StringField('name', validators=[Required()])
+    submit = SubmitField('submit')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['konwn'] = True
         session['name'] = form.name.data
-        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html',
+                            form=form,
+                            name=session.get('name'),
+                            known = session.get('known', False))
+
 
 
 if __name__ == '__main__':
