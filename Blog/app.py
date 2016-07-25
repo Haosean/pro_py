8a1d92a365807a153f5453f161ca84282119ca7d
wangfm <164245460@qq.com>
2016-07-25 09:54:00 +0800

    add

--- Blog/app.py
+++ Blog/app.py
@@ -1,13 +1,18 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return 'Blog'
+    return render_template('base.html')
+
 
 
 if __name__ == '__main__':
