f4ac1abdac9db0695baeb4134241b893136a3d36
wfm <164245260@qq.com>
2016-08-19 11:11:35 +0800

    update

--- 01_python_aotu/webcase/test_login_2.py
+++ 01_python_aotu/webcase/test_login_2.py
@@ -10,6 +10,7 @@ from time import sleep
 
 # http://10.1.0.54/middleclient/pcManager/LessonPage.do?typeCode=1401&lessonId=ad53d86d-24d5-40fc-99c7-3a799983aa00&isZhu=1&classroomId=3b56d0d6-bae4-4e42-b0f7-0534fcbd64c9&interactType=false&created=0&mcuIds=ee8e08a4-7113-404d-ab63-e938ce14c631
 # http://10.1.0.54/middleclient/pcManager/LessonPage.do?typeCode=1401&lessonId=2ef96704-b305-4991-a1c2-b69111a1834e&isZhu=1&classroomId=7a562357-4a68-40b4-bb30-50c7da90512e&interactType=false&created=0&mcuIds=ee8e08a4-7113-404d-ab63-e938ce14c631
+# http://10.1.0.54/middleclient/pcManager/LessonPage.do?typeCode=1401&lessonId=838ff881-e1f3-4515-96c2-7e405c1bbc7c&isZhu=1&classroomId=7a562357-4a68-40b4-bb30-50c7da90512e&interactType=false&created=0&mcuIds=ee8e08a4-7113-404d-ab63-e938ce14c631
 
 
 def login():
@@ -27,24 +28,16 @@ def login():
     driver.find_element_by_id("s_password").send_keys("111111")
     # 登录
     driver.find_element_by_name("submit").click()
+    driver.maximize_window()
     sleep(10)
     # 开课
     driver.find_element_by_id("pc_startButton").click()
 
-    sleep(70)
+    sleep(300)
     # 开课
     driver.find_element_by_id("pc_startButton").click()
-    # pc_startButton
+
     driver.maximize_window()
-    driver.find_element_by_link_text(u"内容管理").click()
-    driver.find_element_by_link_text(u"视频管理").click()
-    driver.find_element_by_id("upload").click()
-    filePath = "G:\\2.mp4"
-    driver.find_element_by_id("upload").sendKeys(filePath)
-    # String filePath = "G:\\2.mp4"
-    # adFileUpload..sendKeys(filePath)
-    sleep(20)
-    driver.find_element_by_id("modeeditsure").click()
     sleep(2)
     sleep(10)
 
